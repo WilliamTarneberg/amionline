970c8f47e57afcd1684b68534c5b5aff667872a7
WilliamTarneberg <william.tarneberg@eit.lth.se>
2014-08-29 13:03:06 +0200

    win vs. rest

--- amionline.py
+++ amionline.py
@@ -1,32 +1,64 @@
 import re
 import subprocess
-website = "google.com"
+import platform
+
+
+
+import sys
 
 def main(argv):
-    # My code here
-    pass
+    os_type = platform.system()
+    target = "google.com"
+
+    if os_type=="linux"||"darwin"
+    	ping_unix(target)
+    else 
+    	ping_win(target)
 
 if __name__ == "__main__":
     main(sys.argv)
 
-def ping(target)
-try:
-    ping = subprocess.Popen(["ping", "-n", "-c 5", target], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, error = ping.communicate()
-    if out:
-        try:
-            minimum = int(re.findall(r"Minimum = (\d+)", out)[0])
-            maximum = int(re.findall(r"Maximum = (\d+)", out)[0])
-            average = int(re.findall(r"Average = (\d+)", out)[0])
-            packet = int(re.findall(r"Lost = (\d+)", out)[0])
-            if packet > 1:
-                packet = 5 / packet * 100
+def ping_win(target)
+	try:
+	    ping = subprocess.Popen(["ping", "-n", "-c 5", target], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	    out, error = ping.communicate()
+	    if out:
+	        try:
+	            minimum = int(re.findall(r"Minimum = (\d+)", out)[0])
+	            maximum = int(re.findall(r"Maximum = (\d+)", out)[0])
+	            average = int(re.findall(r"Average = (\d+)", out)[0])
+	            packet = int(re.findall(r"Lost = (\d+)", out)[0])
+	            if packet > 1:
+	                packet = 5 / packet * 100
 
-            print "average ms"
-        except:
-            print "no data for one of minimum,maximum,average,packet"
-    else:
-        print 'No ping'
+	            print "average ms"
+	        except:
+	            print "no data for one of minimum,maximum,average,packet"
+	    else:
+	        print 'No ping'
 
-except subprocess.CalledProcessError:
-    print "Couldn't get a ping"
+	except subprocess.CalledProcessError:
+	    print "Couldn't get a ping"
+
+def ping_unix(target)
+	try:
+	    ping = subprocess.Popen(["ping", "-n", "-c 5", target], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	    out, error = ping.communicate()
+	    if out:
+	        try:
+	            minimum = int(re.findall(r"Minimum = (\d+)", out)[0])
+	            maximum = int(re.findall(r"Maximum = (\d+)", out)[0])
+	            average = int(re.findall(r"Average = (\d+)", out)[0])
+	            packet = int(re.findall(r"Lost = (\d+)", out)[0])
+	            if packet > 1:
+	                packet = 5 / packet * 100
+
+	            print "average ms"
+	        except:
+	            print "no data for one of minimum,maximum,average,packet"
+	    else:
+	        print 'No ping'
+
+	except subprocess.CalledProcessError:
+	    print "Couldn't get a ping"
+
