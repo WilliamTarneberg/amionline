13c8a85a2187bf4127ebbf94bb826f7d2bb5ae0e
ettmarcus <marcus@Macbook-Retina.local>
2014-08-30 23:41:45 +0200

    good log output

--- ping.sh
+++ ping.sh
@@ -2,16 +2,18 @@
 
 #/bin/ping -n -i 300 8.8.8.8 | while read pong; do echo "$(date): $pong"; done >> /home/pi/ping.log
 
-HOSTS="8.8.8.8 213.100.83.1 10.0.1.1 10.0.1.53 10.0.1.2 10.0.1.51 10.0.1.52 10.0.1.54 10.0.1.55 10.0.1.56"
+#HOSTS="8.8.8.8 213.100.83.1 10.0.1.1 10.0.1.53 10.0.1.2 10.0.1.51 10.0.1.52 10.0.1.54 10.0.1.55 10.0.1.56"
+HOSTS="10.0.1.1 10.0.1.3"
 COUNT=4
 
 for theHost in $HOSTS
 do
-  times=$(/bin/ping -n -c $COUNT $theHost | grep 'rtt' | awk -F' ' '{ print $4 }')
-  if [ "$times" != "" ]
+  time=$(date '+%Y-%m-%d %H:%M:%S')
+  values=$(/sbin/ping -n -c $COUNT $theHost | grep 'min/avg/max' | awk -F' ' '{print $4}' | sed 's/\//,/g')
+  if [ "$values" != "" ]
     then
-      echo "$(date) - PING $theHost OK - $times" >> /home/pi/ping.log
+      echo "$time,$theHost,OK,$values" >> /var/log/amionline.ping.log
     else
-      echo "$(date) - PING $theHost FAILED" >> /home/pi/ping.log
+      echo "$time,$theHost,FAILED,-1,-1,-1,-1" >> /var/log/amionline.ping.log
   fi
 done
